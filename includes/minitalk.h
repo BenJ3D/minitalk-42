c2657906537710aba0c8bad08103b183bd73bc3a
BenJ3D <benjamin@ben3d.fr>
2022-02-21 18:40:18 +0100

    save

---
+++ includes/minitalk.h
@@ -0,0 +1,11 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include "../ft_printf/includes/ft_printf.h"
+
+
+#endif
---
+++ server.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 00:46:48 by bducrocq          #+#    #+#             */
+/*   Updated: 2022/02/21 02:10:40 by bducrocq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minitalk.h"
+#include "ft_printf/includes/ft_printf.h"
+
+int	main()
+{
+	ft_printf("server PID : %d", getpid());
+	while(1)
+	
+	return (0);
+}
