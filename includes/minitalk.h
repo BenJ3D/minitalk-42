a238b3b8ce42c215da0f7e6c4bd8a815ef30df71
Bducrocq <bducrocq42@gmail.com>
2022-02-21 18:49:16 +0100

    save

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -5,7 +5,6 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <stdarg.h>
-# include "../ft_printf/includes/ft_printf.h"
 
 
 #endif
--- server.c
+++ server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 00:46:48 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/02/21 02:10:40 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/02/21 18:49:07 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 int	main()
 {
-	ft_printf("server PID : %d", getpid());
-	while(1)
-	
+	ft_printf("server PID : %d\n", getpid());
 	return (0);
 }
