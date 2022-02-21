c662f69f35b78fceb3a14de676b667d935cc11ad
Bducrocq <bducrocq42@gmail.com>
2022-02-21 18:56:39 +0100

    save

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -5,6 +5,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <stdarg.h>
+# include "../ft_printf/includes/ft_printf.h"
 
 
 #endif
--- server.c
+++ server.c
@@ -6,12 +6,11 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 00:46:48 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/02/21 18:49:07 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/02/21 18:55:27 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minitalk.h"
-#include "ft_printf/includes/ft_printf.h"
 
 int	main()
 {
