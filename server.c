a47e6c9f9843e0fc6623c0dadd836d9cbac738ef
Bducrocq <bducrocq42@gmail.com>
2022-02-22 18:58:16 +0100

    save

--- server.c
+++ server.c
@@ -6,16 +6,23 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 00:46:48 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/02/21 19:41:26 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/02/22 18:57:57 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minitalk.h"
 
+// void	sigusr_process()
+
+
 int	main()
 {
 	struct sigaction sa;
-
 	ft_printf("server PID : %d\n", getpid());
+	
+
+	
+	while (1)
+		pause();
 	return (0);
 }
