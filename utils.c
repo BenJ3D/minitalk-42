1210f5812282d3d4c0b3dc99aade4f1589e72b2a
Bducrocq <bducrocq42@gmail.com>
2022-02-24 19:07:40 +0100

    save

--- utils.c
+++ utils.c
@@ -3,40 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 19:44:26 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/02/23 23:51:55 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/02/24 19:07:22 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minitalk.h"
 
+/**
+ * @brief pass char to binary
+ * 
+ * @param c 
+ * @return char* 
+ */
 char	*ft_atob(char c)
 {
 	int	i;
 	int	base;
-	char str[8];
+	char *str;
 
+	str = malloc(sizeof(char) * 9);
+	if(!str)
+		return (NULL);
 	base = 128;
-
 	i = 0;
 	printf("%d %d %d\n", c, (c / 128), (c % 128));
-	while (i < 8)
+	while (base >= 1)
 	{
-		if((c / base) == 1) // diviser par base qui se divise par 2 a chaque tour
-			str[i++] = 1;
+		if((c / base) == 1)
+		{
+			str[i] = '1';
+			c = c - base;
+		}
 		else 
-		str[i++] = 0;
-			
+			str[i] = '0';
+		i++;	
+		if (base == 1)
+			break;
+		base = base / 2;
 	}
+	str[i] = '\0';
 	return(str);
 }
 
 
-int main()
-{
+int main(int argc, char **argv)
+{	
+	char *str;
 
-	ft_atob('L');
+	str = ft_atob(argv[1][0]);
+	printf("%s", str);
+	free(str);
 	return(0);
 }
