e45efe7cc1539a7f09a209fe85e6b00d8c61f684
Bducrocq <bducrocq42@gmail.com>
2022-03-15 17:04:24 +0100

    save

--- .run/runsanitize.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-int status = system("gcc ./libft/ft_putstr_fd.o ./libft/ft_calloc.o ./libft/ft_bzero.o ./libft/ft_memset.o  utils.c server.c -o server");                   //gf
-// int status = system("gcc *.c libft/libft.a && ./a.out");                   //gf
-// int status = system("gcc *.c libftprintf.a && ./a.out");
-}
